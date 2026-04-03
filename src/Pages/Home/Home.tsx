3ef4295fdb24bad36ce4558f4e9c04ad316ede69
Brice Kutuk <bricekutuk@live.fr>
2026-04-03 14:54:39 +0200

    bug restart computer

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,11 +1,16 @@
 import './Home.css'
 import CeHome from '../../assets/Images/VapePageHome.jpg'
-
+import '@fontsource/dm-serif-display/400.css';
+import '@fontsource/dm-sans/400.css';
+import '@fontsource/dm-sans/500.css';
 
 function Home() {
   return   <>
-  <h1>Produit phare</h1>
+  <h1 className='phraseAccroche'>Votre référence en matière de vape. Des produits certifiés, des conseils experts, une expérience sans compromis.</h1>
   <img src={CeHome} alt="CigaretteElectroniqueEnHautPage" className='CEEnTete'/>
+  <p className='paraAccroche'>Nouveau ou expérimenté, il y a toujours quelque chose à découvrir. Plongez dans notre catalogue de dispositifs soigneusement sélectionnés ou explorez nos saveurs les plus populaires — et laissez la qualité parler d'elle-même.</p>
+  <button type='button' className='btnEgarette'>Voir les E-garette</button>
+  <button type='button' className='btnSaveurs'>Explorer les saveurs</button>
   </>
 }
 
